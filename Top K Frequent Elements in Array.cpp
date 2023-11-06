0d64e25631850ccdaa17e10b8fcd9f1d69113138
SoumyaDeori <131858497+SoumyaDeori@users.noreply.github.com>
2023-11-06 13:58:56 +0530

    Create Top K Frequent Elements in Array.cpp
    Given a non-empty array nums[] of integers of length N, find the top k elements which have the highest frequency in the array. If two numbers have same frequencies, then the larger number should be given more preference.

---
+++ Top K Frequent Elements in Array.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    vector<int> topK(vector<int> &nums, int k)
+    { // Code here unordered_map<int, int> frequencyMap; 
+    for (int num : nums) { frequencyMap[num]++; } 
+    multiset<pair<int, int>> sortedSet;
+     for (auto entry : frequencyMap)
+      { sortedSet.insert({entry.second, entry.first}); 
+      if (sortedSet.size() > k) { sortedSet.erase(sortedSet.begin()); } } 
+      vector<int> result; 
+      for (auto it = sortedSet.rbegin(); it != sortedSet.rend(); ++it) 
+      { result.push_back(it->second); } return result; } 
+      };
